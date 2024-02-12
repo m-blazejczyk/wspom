386c890453fce615ac8e99d0c37c268336fd9d30
Michal Blazejczyk <mblazejczyk@ei3.com>
2024-02-11 21:28:13 -0500

    Fixed the auto-generated display template for entries

--- lib/wspom/entry.ex
+++ lib/wspom/entry.ex
@@ -1,4 +1,15 @@
 defmodule Wspom.Entry do
   defstruct [:id, :description, :title, :year, :month, :day, :weekday,
     importance: :normal, fuzzy: 0, needs_review: false, tags: MapSet.new()]
+
+  def tags_to_string(tags) do
+    if MapSet.size(tags) > 0 do
+      tags
+      |> MapSet.to_list()
+      |> Enum.sort()
+      |> Enum.join(', ')
+    else
+      ''
+    end
+  end
 end

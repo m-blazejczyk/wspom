34228b9295c90527a1789a6e9fabba40417ad028
Michal Blazejczyk <michal.blazejczyk@gmail.com>
2024-12-21 23:25:17 -0500

    Proper tests for the first phase of tags_from_string

--- test/tags_and_cascades_test.exs
+++ test/tags_and_cascades_test.exs
@@ -3,7 +3,11 @@ defmodule TagsAndCascadesTest do
 
   alias Wspom.TnC
 
-  test "Testing the tests" do
+  test "Tags from string" do
     assert TnC.tags_from_string("") == {:ok, %{tags: MapSet.new([])}}
+    assert TnC.tags_from_string("t1 t4") == {:ok, %{tags: MapSet.new(["t1", "t4"])}}
+    assert TnC.tags_from_string("t1 c") == {:ok, %{tags: MapSet.new(["t1", "t2", "c"])}}
+    assert TnC.tags_from_string("c") == {:ok, %{tags: MapSet.new(["t1", "t2", "c"])}}
+    assert TnC.tags_from_string("t4 c") == {:ok, %{tags: MapSet.new(["t1", "t2", "c", "t4"])}}
   end
 end

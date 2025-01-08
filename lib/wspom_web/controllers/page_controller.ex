b89a823206f56a91fa232f87c7f871fe2caf4004
Michal Blazejczyk <michal.blazejczyk@gmail.com>
2025-01-08 13:43:13 -0500

    Modified the home page to include cards linking to all three future sections (Wspom, Weight and Books)

--- lib/wspom_web/controllers/page_controller.ex
+++ lib/wspom_web/controllers/page_controller.ex
@@ -2,9 +2,7 @@ defmodule WspomWeb.PageController do
   use WspomWeb, :controller
 
   def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    # render(conn, :home, layout: false)
-    redirect(conn, to: ~p"/entries")
+    # The home page is often custom made - skip the default app layout.
+    render(conn, :home, layout: false)
   end
 end

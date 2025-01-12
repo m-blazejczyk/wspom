fc67a152c012677908e62b6fe03ca6c310ae1077
Michal Blazejczyk <michal.blazejczyk@gmail.com>
2025-01-11 23:27:02 -0500

    Functional < and > buttons on the Add Weight form

--- lib/wspom_web/live/weight_edit_form.ex
+++ lib/wspom_web/live/weight_edit_form.ex
@@ -20,20 +20,59 @@ defmodule WspomWeb.Live.WeightEditForm do
         </.header>
 
         <div class="flex gap-2 ">
-          <.button type="button" class="flex-none w-20" phx-click={JS.toggle(to: "#all-tags")}>
+          <.button type="button" class="flex-none w-20" phx-click={JS.push("day_earlier")} phx-target={@myself}>
             &lt;
           </.button>
           <div class="flex-auto">
-            <.input field={@form[:date]} type="text"
+            <.input field={@form[:date]} type="text" class="text-xl text-center"
               class_container="flex items-start flex-col justify-start"/>
           </div>
-          <.button type="button" class="flex-none w-20" phx-click={JS.toggle(to: "#all-cascades")}>
+          <.button type="button" class="flex-none w-20" phx-click={JS.push("day_later")} phx-target={@myself}>
             &gt;
           </.button>
         </div>
 
-        <.input field={@form[:weight]} type="number"
+        <.input field={@form[:weight]} type="number" class="text-xl"
           class_container="flex items-start flex-col justify-start"/>
+
+        <div class="flex flex-wrap rounded-lg bg-gray-300 max-w-sm mx-auto mt-24">
+          <div class="w-1/3">
+            <button class="w-full h-20 text-xl text-gray-700 rounded-lg hover:bg-gray-400">1</button>
+          </div>
+          <div class="w-1/3">
+            <button class="w-full h-20 text-xl text-gray-700 rounded-lg hover:bg-gray-400">2</button>
+          </div>
+          <div class="w-1/3">
+            <button class="w-full h-20 text-xl text-gray-700 rounded-lg hover:bg-gray-400">3</button>
+          </div>
+          <div class="w-1/3">
+            <button class="w-full h-20 text-xl text-gray-700 rounded-lg hover:bg-gray-400">4</button>
+          </div>
+          <div class="w-1/3">
+            <button class="w-full h-20 text-xl text-gray-700 rounded-lg hover:bg-gray-400">5</button>
+          </div>
+          <div class="w-1/3">
+            <button class="w-full h-20 text-xl text-gray-700 rounded-lg hover:bg-gray-400">6</button>
+          </div>
+          <div class="w-1/3">
+            <button class="w-full h-20 text-xl text-gray-700 rounded-lg hover:bg-gray-400">7</button>
+          </div>
+          <div class="w-1/3">
+            <button class="w-full h-20 text-xl text-gray-700 rounded-lg hover:bg-gray-400">8</button>
+          </div>
+          <div class="w-1/3">
+            <button class="w-full h-20 text-xl text-gray-700 rounded-lg hover:bg-gray-400">9</button>
+          </div>
+          <div class="w-1/3">
+            <button class="w-full h-20 text-xl text-gray-700 rounded-lg hover:bg-gray-400">•</button>
+          </div>
+          <div class="w-1/3">
+            <button class="w-full h-20 text-xl text-gray-700 rounded-lg hover:bg-gray-400">0</button>
+          </div>
+          <div class="w-1/3">
+            <button class="w-full h-20 text-xl text-gray-700 rounded-lg hover:bg-gray-400">⌫</button>
+          </div>
+        </div>
 
         <:actions>
           <.button phx-disable-with="Saving…" class="w-full">Save</.button>
@@ -66,7 +105,7 @@ defmodule WspomWeb.Live.WeightEditForm do
   def update(%{data: nil} = assigns, socket) do
     # This variant is called when the "Add Weight" form is initialized
     now = Timex.now("America/Montreal") |> DateTime.to_date() |> Date.to_string()
-    default_data = %{weight: nil, date: now}
+    default_data = %{date: now, weight: ""}
     update(assigns |> Map.replace(:data, default_data), socket)
   end
   def update(%{data: data} = assigns, socket) do
@@ -83,9 +122,36 @@ defmodule WspomWeb.Live.WeightEditForm do
     changeset = to_changeset(socket.assigns.data, form_params)
     {:noreply, assign(socket, form: to_form(changeset, action: :validate, as: "data"))}
   end
-
   def handle_event("save", %{"data" => form_params}, socket) do
     save_entry(socket, socket.assigns.action, form_params)
+  end
+  def handle_event("day_earlier", _, socket) do
+    add_days_to_date(socket, -1)
+  end
+  def handle_event("day_later", _, socket) do
+    add_days_to_date(socket, 1)
+  end
+
+  defp add_days_to_date(socket, days) do
+    modify_field(socket, "date", fn current_date ->
+      with {:ok, date} <- Date.from_iso8601(current_date) do
+        date |> Date.add(days) |> to_string()
+      else
+        _ -> current_date
+      end
+    end)
+  end
+
+  defp modify_field(socket, field, fun) do
+    old_value = Map.get(socket.assigns.form.params, field)
+      || Map.get(socket.assigns.form.data, String.to_existing_atom(field))
+    new_value = fun.(old_value)
+
+    # I am not sure if this is the idiomatic way of handling this…
+    # But based on what I found on the internet and tried out myself,
+    # this is the most effective way of making dynamic changes for form fields.
+    new_params = Map.put(socket.assigns.form.params, field, new_value)
+    handle_event("validate", %{"data" => new_params}, socket)
   end
 
   defp save_entry(socket, :edit, form_params) do

098c16c389cff704cf28d61f144bcf6785dc6e97
Michal Blazejczyk <michal.blazejczyk@gmail.com>
2025-02-14 23:33:16 -0500

    Formatting change

--- lib/wspom_web/controllers/page_controller.ex
+++ lib/wspom_web/controllers/page_controller.ex
@@ -2,11 +2,14 @@ defmodule WspomWeb.PageController do
   use WspomWeb, :controller
 
   def home(conn, _params) do
-    # The home page is often custom made - skip the default app layout.
     entry_stats = Wspom.Entries.Context.get_stats()
     weight_stats = Wspom.Weight.Context.get_stats()
-    render(conn, :home, layout: false,
+    render(
+      conn, :home,
+      # The home page is often custom made - skip the default app layout.
+      layout: false,
       entries: "#{entry_stats.entries} entries",
-      days: "#{weight_stats.days} days")
+      days: "#{weight_stats.days} days"
+    )
   end
 end

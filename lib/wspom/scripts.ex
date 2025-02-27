e59d59d461263c232cbbd826f96d26db6187e20f
Michal Blazejczyk <michal.blazejczyk@gmail.com>
2025-02-26 23:07:10 -0500

    Script to parse text files containing entries: basic structure + validation

--- lib/wspom/scripts.ex
+++ lib/wspom/scripts.ex
@@ -9,7 +9,22 @@ defmodule Wspom.Scripts do
   Wspom.Database.append_entries_and_save(entries)
   """
 
-  def entries_from_json(filename) do
+@roman_lookup %{
+  "i" => 1,
+  "ii" => 2,
+  "iii" => 3,
+  "iv" => 4,
+  "v" => 5,
+  "vi" => 6,
+  "vii" => 7,
+  "viii" => 8,
+  "ix" => 9,
+  "x" => 10,
+  "xi" => 11,
+  "xii" => 12,
+}
+
+def entries_from_json(filename) do
     # We expect `json` to be a list of maps
     {:ok, json} = read_json(filename)
 
@@ -57,4 +72,82 @@ defmodule Wspom.Scripts do
       IO.puts("File #{filename} created")
     end
   end
+
+  # Wspom.Scripts.load_entries("/home/michal/Documents/Wspom/test.txt", 2025)
+  def load_entries(filename, year) do
+    {entries_raw, _} = File.read!(filename)
+    |> String.split("\n")
+    |> Enum.map(&String.trim/1)
+    |> Enum.chunk_while([], &chunker/2, &wrapup/1)
+    |> Enum.map_reduce(year, &validate_entry/2)
+
+    errors = entries_raw |> Enum.filter(&find_errors/1)
+
+    if length(errors) > 0 do
+      IO.puts("Errors encountered:")
+
+      errors
+      |> Enum.map(fn {:error, error} -> error end)
+      |> Enum.each(&IO.puts/1)
+
+      nil
+    else
+      entries_raw
+    end
+  end
+
+  defp chunker(elem, acc) do
+    # This looks for a line composed of two or more dashes;
+    # when it finds one, the accumulator is emitted as a chunk
+    # and a new, empty list is returned as the new accumulator
+    if elem |> String.match?(~r/^-[-]+$/) do
+      {:cont, Enum.reverse(acc), []}
+    else
+      # If it's not the dashes then add the line to the accumulator
+      # but only if it is not empty
+      if String.length(elem) > 0 do
+        {:cont, [elem | acc]}
+      else
+        {:cont, acc}
+      end
+    end
+  end
+
+  # This function is called at the very end of the file
+  # It simply emits the accumulator
+  defp wrapup(acc), do: {:cont, Enum.reverse(acc), []}
+
+  defp validate_entry([date_str | [title_raw | content]], year) do
+    with {:ok, date} <- convert_roman_date(date_str, year),
+         {:ok, title} <- validate_title(title_raw) do
+      {{date, title, Enum.join(content, "\n\n")}, year}
+    else
+      {:error, error} -> {{:error, error}, year}
+    end
+  end
+
+  # Returns a {:ok, Date} or {:error, "Error message"}
+  defp convert_roman_date(date_str, year) do
+    # This will throw an exception if `date_str` is not
+    [day_str | [month_roman | []]] = date_str |> String.split(" ")
+
+    with {day, ""} <- Integer.parse(day_str),
+         {:ok, month} <- @roman_lookup |> Map.fetch(month_roman |> String.downcase()),
+         {:ok, date} <- Date.new(year, month, day) do
+      {:ok, date}
+    else
+      _ -> {:error, "Incorrect date: #{date_str}"}
+    end
+  end
+
+  defp validate_title(title) do
+    if String.length(title) > 50 do
+      {:error, "Title too long: #{title}"}
+    else
+      {:ok, title}
+    end
+  end
+
+  defp find_errors({:error, _}), do: true
+  defp find_errors(_), do: false
 end

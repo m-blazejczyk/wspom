b90444c3f1a7487e74e9d60a40fdbf0c0d6e0173
Michal Blazejczyk <michal.blazejczyk@gmail.com>
2025-09-08 22:41:48 -0400

    Completed the calculation of data for the weight chart

--- lib/wspom/weight/weight_chart_data.ex
+++ lib/wspom/weight/weight_chart_data.ex
@@ -6,21 +6,22 @@ defmodule Wspom.WeightChart.Data do
   # r Wspom.WeightChart.Data
   # all = Wspom.Weight.Database.get_all_records()
   # all = all |> Enum.sort(fn w_rec1, w_rec2 -> Date.compare(w_rec1.date, w_rec2.date) == :gt end)
-  # Wspom.WeightChart.Data.make_from_weights(all |> Enum.take(90), 0, 0, 800, 200)
+  # all = all |> Enum.take(90)
+  # Wspom.WeightChart.Data.make_from_weights(all, 0, 0, 800, 200)
   #
   # Prerequisite: `weights` must be sorted by date
   def make_from_weights(weights, x, y, w, h) do
     # Y ticks are easy: hard-coded
     yticks = 81..89
     |> Enum.map(fn w ->
-      w_perc = (w - 80) / 10.0
+      h_perc = (w - 80) / 10.0
       %TickY{
-        pos: round(h * (1.0 - w_perc)) + y,
+        pos: round(h * (1.0 - h_perc)) + y,
         text: Integer.to_string(w)}
     end)
 
     # Let's see what date range we're dealing with
-    {earliest, latest, diff} =
+    {earliest, latest, date_range} =
       with fst <- hd(weights).date,
         lst <- List.last(weights).date,
         diff <- Date.diff(fst, lst)
@@ -36,10 +37,10 @@ defmodule Wspom.WeightChart.Data do
     # one tick per month; assume 50 pixels per month is the minimum
     effective_width = w - 10
     tick_every_x_months =
-      if effective_width / (diff / 30) >= 50.0 do
+      if effective_width / (date_range / 30) >= 50.0 do
         1
       else
-        if effective_width / (diff / 60) >= 50.0 do
+        if effective_width / (date_range / 60) >= 50.0 do
           2
         else
           3
@@ -57,6 +58,27 @@ defmodule Wspom.WeightChart.Data do
     # of the chart. The only exception is if the first weight record was
     # taken on the first day of a month.
     tick_dates = if earliest.day == 1, do: tick_dates, else: remaining_dates
+
+    xticks = tick_dates
+    |> Enum.map(fn tick_date ->
+      w_perc = Date.diff(tick_date, earliest) / date_range
+      %TickX{
+        pos: round(effective_width * w_perc) + x + 5,
+        text_up: Calendar.strftime(tick_date, "%b"),
+        text_down: Integer.to_string(tick_date.year)}
+    end)
+
+    points = weights
+    |> Enum.map(fn %{date: date, weight: weight} ->
+      w_perc = Date.diff(date, earliest) / date_range
+      h_perc = (weight - 80) / 10.0
+      {
+        round(effective_width * w_perc) + x + 5,
+        round(h * (1.0 - h_perc)) + y
+      }
+    end)
+
+    {xticks, yticks, points}
   end
 
   def add_tick([latest | _rest] = list, tick_every_x_months, stop_after) do
